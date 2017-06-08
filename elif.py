619aba0a1fb0998ab84ebbb6813f0e0216940c41
Daniel <oralordos@gmail.com>
2017-06-08 14:42:58 -0700

    More conditionals

---
+++ elif.py
@@ -0,0 +1,10 @@
+x = 120
+if x > 3:
+    print('Our number is big')
+    x = 10
+elif x > 0:
+    print('Our number is negative')
+else:
+    print('Our number is small')
+    
+print('X is ' + str(x))
---
+++ inline.py
@@ -0,0 +1,5 @@
+print('Enter a number')
+x = int(input())
+extraS = '' if x == 1 else 's'
+
+print(str(x) + ' dog' + extraS)
