8195f8c6ea5de61b0d827f580e760426e2f37ad2
Daniel <oralordos@gmail.com>
2017-06-14 14:07:01 -0700

    Add range function code

---
+++ 024-Range-Function/range-function.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print(i)
+
+print('\n\n')
+
+for i in range(2, 5):
+    print(i)
+    
+print('\n\n')
+
+for i in range(0, 100, 10):
+    print(i)
+
+print('\n\n')
+
+for i in range(100, 0, -10):
+    print(i)
