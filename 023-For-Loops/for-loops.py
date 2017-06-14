5f4effaa25296d915d0acb5c4b3c2f8de0aaaba4
Daniel <oralordos@gmail.com>
2017-06-14 13:52:19 -0700

    Add basic for loop examples

---
+++ 023-For-Loops/for-loops.py
@@ -0,0 +1,16 @@
+groceries = ['Apples', 'Bread', 'Cookies']
+for item in groceries:
+    print(item)
+    print('-----')
+
+print('\n\n')
+
+people = ('Mark', 'John', 'Polly')
+for person in people:
+    print(person)
+
+print('\n\n')
+
+thing = {'name': 'Bob', 'height': 70, 'weight': '100lb'}
+for key in thing:
+    print(thing[key])
