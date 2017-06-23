dd47ff8b58e1c7ad599f0c7deffbf4b145983e26
Daniel <oralordos@gmail.com>
2017-06-23 11:07:57 -0700

    Add default parameters example

---
+++ 032-Default-Parameters/default_parameters.py
@@ -0,0 +1,7 @@
+def multiply(val, by = 2):
+    return val * by
+
+print('5, 5: {}'.format(multiply(5, 5)))
+print('5: {}'.format(multiply(5)))
+print('val = 5, by = 5: {}'.format(multiply(val = 5, by = 5)))
+print('by = 10, val = 4: {}'.format(multiply(by = 10, val = 4)))
