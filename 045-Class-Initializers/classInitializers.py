fcc281961f9efd015433133e329ffe5b185cc5e0
Daniel <oralordos@gmail.com>
2017-06-28 15:48:55 -0700

    Added class initializers example

---
+++ 045-Class-Initializers/classInitializers.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __del__(self):
+        print('{} was deleted'.format(self.name))
+
+    def sayHello(self):
+        print('Hello, my name is {}'.format(self.name))
+
+p = Person('Bob', 27)
+o = Person('Joe', 15)
+
+p.sayHello()
+o.sayHello()
